8cb494fcffcdeb63edfbcb8af80c663523399a19
prakashivb <prakashivb@yahoo.com>
2015-12-19 16:09:34 -0500

    modified knitter

--- knitter.R
+++ knitter.R
@@ -1,4 +1,3 @@
 library(knitr)
 
-setwd("/Users/pinugant/Personal/Tutorials/Coursera/ReproducibleResearch/RepData_PeerAssessment1")
 knit2html(input = "PA1_template.Rmd", output = "PA1_template.html")
